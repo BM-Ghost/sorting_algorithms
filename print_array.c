341fddf606aa92d4fa9863d325e8b97178019628
Meshack Bwire <b.meshack@outlook.com>
2024-01-23 18:15:02 +0300

    0-0

--- print_array.c
+++ print_array.c
@@ -1,24 +1,23 @@
+#include <stdlib.h>
 #include <stdio.h>
 
 /**
  * print_array - Prints an array of integers
+ *
  * @array: The array to be printed
- * @size: The size of the array
- * * Return: Array
+ * @size: Number of elements in @array
  */
 void print_array(const int *array, size_t size)
 {
-	size_t i;
+    size_t i;
 
-	if (array == NULL || size == 0)
-		return;
-
-	for (i = 0; i < size; i++)
-	{
-		printf("%d", array[i]);
-
-		if (i < size - 1)
-			printf(", ");
-	}
-	printf("\n");
+    i = 0;
+    while (array && i < size)
+    {
+        if (i > 0)
+            printf(", ");
+        printf("%d", array[i]);
+        ++i;
+    }
+    printf("\n");
 }
