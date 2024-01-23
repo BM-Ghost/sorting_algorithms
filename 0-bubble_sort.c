82120d8f427aa9ed4a5b1463726c2dfaa2dee0a5
Meshack Bwire <b.meshack@outlook.com>
2024-01-23 17:37:48 +0300

    bubble sort correction swap

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,31 +1,37 @@
 #include "sort.h"
 
 /**
- * bubble_sort - Sorts an array of integers in ascending order using Bubble sort
- * @array: The array to be sorted
- * @size: The size of the array
- * Return: Nothing.
+ * swap_int - Swaps two integers.
+ * @a: The first integer.
+ * @b: The second integer.
+ * Return: void.
+ */
+void swap_int(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+/**
+ * bubble_sort - Sorts an array using the Bubble Sort algorithm.
+ * @array: The array to sort.
+ * @size: The size of the array.
+ * Return: void.
  */
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, j;
-	int temp;
+    size_t i, j;
 
-	if (array == NULL || size < 2)
-		return;
-
-	for (i = 0; i < size - 1; i++)
-	{
-		for (j = 0; j < size - i - 1; j++)
-		{
-			if (array[j] > array[j + 1])
-			{
-				temp = array[j];
-				array[j] = array[j + 1];
-				array[j + 1] = temp;
-
-				print_array(array, size);
-			}
-		}
-	}
+    for (i = 0; i < size; i++)
+    {
+        for (j = 0; j < (size - i - 1); j++)
+        {
+            if (array[j + 1] < array[j])
+            {
+                swap_int(&array[j + 1], &array[j]);
+                print_array(array, size);
+            }
+        }
+    }
 }
