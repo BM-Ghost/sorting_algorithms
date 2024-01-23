02cdcef9103ab8e9ec2d27214571c13a8ff0a6a9
Meshack Bwire <b.meshack@outlook.com>
2024-01-23 23:55:32 +0300

    shell sort

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,41 +1,34 @@
 #include "sort.h"
 
 /**
- * shell_sort - function that sorts an array of integers
- * in ascending order using the Shell sort algorithm,
- * using the Knuth sequence.
- *
- * @array: array of integers
- * @size: size of the array.
+ * shell_sort - function that implement an a quick form
+ *		of insertiong sort
+ * @array: the array list ot item
+ * @size: the size of the array
  */
 
 void shell_sort(int *array, size_t size)
 {
-	size_t interval;
+	size_t gap = 1, j, i;
 	int temp;
-	size_t i, j;
 
-	interval = 1;
+	if (!array && size < 2)
+		return;
+	while (gap <= size / 3)
+		gap = gap * 3 + 1;
 
-	while (interval <= size / 3)
+	while (gap > 0)
 	{
-		interval = interval * 3 + 1;
-	}
-	while (interval > 0)
-	{
-		for (i = interval; i < size; i++)
+		for (i = gap; i < size; i++)
 		{
 			temp = array[i];
-			j = i;
-			while (j >= interval && array[j - interval] > temp)
-			{
-				array[j] = array[j - interval];
-				j -= interval;
-			}
+			for (j = i; j >= gap && array[j - gap] > temp; j -= gap)
+				array[j] = array[j - gap];
+
 			array[j] = temp;
+
 		}
 		print_array(array, size);
-
-		interval = (size_t)(interval - 1) / 3;
+		gap = (gap - 1) / 3;
 	}
 }
