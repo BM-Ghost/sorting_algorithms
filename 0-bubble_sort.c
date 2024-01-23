3b580f23266be5aed73adff549c09084627a19cb
Meshack Bwire <b.meshack@outlook.com>
2024-01-23 18:18:41 +0300

    0-0 bubble-sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -2,33 +2,25 @@
 
 /**
  * bubble_sort - Sorts an array of integers in ascending order using Bubble sort
- * @array: The array to be sorted
- * @size: Number of elements in the array
+ * @array: array of integers.
+ * @size: size of array.
  */
 void bubble_sort(int *array, size_t size)
 {
-    size_t i, j;
-    int temp;
-    int swapped;
+	size_t i, j;
+	int swap;
 
-    for (i = 0; i < size - 1; ++i)
-    {
-        swapped = 0;
-
-        for (j = 0; j < size - i - 1; ++j)
-        {
-            if (array[j] > array[j + 1])
-            {
-                temp = array[j];
-                array[j] = array[j + 1];
-                array[j + 1] = temp;
-                swapped = 1;
-
-                print_array(array, size);
-            }
-        }
-
-        if (swapped == 0)
-            break;
-    }
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				swap = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = swap;
+				print_array(array, size);
+			}
+		}
+	}
 }
