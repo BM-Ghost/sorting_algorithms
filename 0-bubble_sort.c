812abe48a0597355956d684e79eab9d05691cadb
Meshack Bwire <b.meshack@outlook.com>
2024-01-23 16:53:17 +0300

    bubble sort correction

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -19,7 +19,6 @@ void bubble_sort(int *array, size_t size)
 		{
 			if (array[j] > array[j + 1])
 			{
-				/* Swap array[j] and array[j + 1] */
 				temp = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
